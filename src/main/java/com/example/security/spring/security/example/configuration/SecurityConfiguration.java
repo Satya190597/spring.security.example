b1e3dc151b9c4a9b9e3665fc5596775c5a6a754b
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2023-05-15 19:59:49 +0530

    In-memory Authentication

---
+++ src/main/java/com/example/security/spring/security/example/configuration/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.security.spring.security.example.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> {
+            // + All the endpoints should be protected.
+            requests.anyRequest().authenticated();
+        }).formLogin(form -> {
+            // + Permit everyone to view the login page.
+            form.permitAll();
+        }).logout(logout -> {
+            // + Permit everyone to view the logout page.
+            logout.permitAll();
+        });
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
